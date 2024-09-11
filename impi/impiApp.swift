a9f9ce78375df2de46291cf3c2ca6b15af80accb
zbornak <zbornak@protonmail.com>
2024-09-11 09:30:21 +0100

    Initial Commit

---
+++ impi/impiApp.swift
@@ -0,0 +1,17 @@
+//
+//  impiApp.swift
+//  impi
+//
+//  Created by Mark Strijdom on 11/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct impiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
